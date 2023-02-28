c809cfb13c1f27ab66e552daa32facdc79ea02c3
ADASC69 <101396240+ADASC69@users.noreply.github.com>
2023-02-28 23:19:06 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+﻿<?php    
+
+    $query = @unserialize (file_get_contents('http://ip-api.com/php/'));
+    if ($query && $query['status'] == 'success') {
+        echo 'Uzytkownik z  ' . $query['country'] . ', ' . $query['city'] . '!'."\n";
+		
+    }
+   
+    if($query && ($query['country'] == 'China'|| $query['country'] == 'Russia'|| $query['country'] == 'Indonesia'|| $query['country'] == 'Poland')){
+		
+		die("chuj");
+	}
+	
+?>
